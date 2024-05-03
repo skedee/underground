a14e3a8048e4d344a3d8aca5b835b9947e5b1f85
Chad Oliver <chad@avtal.com>
2024-05-03 13:03:15 -0500

    update makefiles

--- feature/makefile/.makefile/common.mk
+++ feature/makefile/.makefile/common.mk
@@ -7,7 +7,22 @@ RED = \033[91m
 YELLOW = \033[93m
 CYAN = \033[36m
 
+OS = $(shell uname -s)
+
 .PHONY: BLUE GREEN NC PURPLE RED YELLOW CYAN FOREACH PRINT_MENU
+
+# load .env variables 
+ifneq ("$(wildcard .env)","")
+    include .env
+    export
+endif
+
+# is the OS using ARM
+ifeq (,$(findstring arm,$(shell uname -m)))
+	export IS_ARM=false
+else
+	export IS_ARM=true
+endif
 
 define FOREACH
 	for DIR in $(1); do \
