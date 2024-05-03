bdcfb1509a8a5f938f514eee857c7ad28c9b20b7
Chad Oliver <chad@avtal.com>
2024-05-03 13:36:27 -0500

    go version

--- feature/sqlc/install.go
+++ feature/sqlc/install.go
@@ -10,16 +10,16 @@ import (
 
 func Install(content embed.FS) {
 	if flags.Sqlc {
-		fileNames := []string{"sqlc.yaml", "README.md"}
-		srcDir := "feature/sqlc"
-		destDir := filepath.Join(flags.Project, "sqlc")
-		os.CopyEmbedFiles(content, fileNames, srcDir, destDir)
-
 		srcFile := filepath.Join("feature/sqlc", "makefile")
 		destFile := filepath.Join(flags.Project, "makefile")
 		os.MergeEmbedFile(content, makefile.Keywords, srcFile, destFile)
 
 		os.Mkdir(os.GetPath(flags.Project, "sqlc/db"))
 		os.Mkdir(os.GetPath(flags.Project, "sqlc/query"))
+
+		fileNames := []string{"sqlc.yaml", "README.md"}
+		srcDir := "feature/sqlc"
+		destDir := filepath.Join(flags.Project, "sqlc")
+		os.CopyEmbedFiles(content, fileNames, srcDir, destDir)
 	}
 }
